d55a1baa09feb55c26802343ba5c44b4c4b87f84
peeyush-jain60 <62890098+peeyush-jain60@users.noreply.github.com>
2021-03-11 13:35:41 +0530

    Linked to mongo atlas and created collections

--- backend/stakeholder/admin.py
+++ backend/stakeholder/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+# all club models
+admin.site.register(BIOBYTES)
+admin.site.register(FOOBAR)
+admin.site.register(BYLD)
+admin.site.register(ASTRONUTS)
+admin.site.register(AUDIOBYTES)
+admin.site.register(MACHAN)
+admin.site.register(MADTOES)
+admin.site.register(ELECTROHOLICS)
+admin.site.register(TASVEER)
+admin.site.register(TRIVIALIS)
+admin.site.register(THE65SQUARE)
+admin.site.register(PHILOSOC)
+admin.site.register(ROBOTICS)
+admin.site.register(KUBIC)
+
