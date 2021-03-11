916b8f2d93163b764a3ee0d5cd84047feebd4363
Chokerino <bhavay18384@iiitd.ac.in>
2021-03-11 19:20:14 +0530

    club page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 
 //import App from "./home-page/App";
 //import App from "./landing-page/App";
-import App from "./events-page/App";
+//import App from "./events-page/App";
+import App from "./club-page/App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
