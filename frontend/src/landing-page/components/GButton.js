5101c9cbe1df3a694191d51b9dd859c82693938f
Chokerino <bhavay18384@iiitd.ac.in>
2021-03-12 15:35:00 +0530

    added login request to django

--- frontend/src/landing-page/components/GButton.js
+++ frontend/src/landing-page/components/GButton.js
@@ -2,24 +2,25 @@ import { FcGoogle } from 'react-icons/fc';
 import { Button, Center, Text } from '@chakra-ui/react';
 import React, { Component }  from 'react';
 import axios from "axios";
+import GoogleLogin from 'react-google-login';
+import googleLogin from "./googleLogin"
 
 export default function GoogleButton() {
     return (
       
       <Center p={8}>
-        <Button
-          w='full'
-          maxW='md'
-          variant='outline'
-          leftIcon={<FcGoogle />}  onClick={() => {console.log("Sending login request")
-                                                    axios.put("http://localhost:8000/admin/",
-                                                    {'login': 0,}
-                                                    );
-                                                  }}>
-          <Center>
-            <Text>Sign in with Google</Text>
-          </Center>
-        </Button>
+        
+        <GoogleLogin
+        clientId="<GOOGLE CLIENT ID>"
+        buttonText="Sign in with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
       </Center>
     );
+  }
+const responseGoogle = async(response) => {
+    let googleResponse  = await googleLogin(response.accessToken)
+    console.log(googleResponse);
+    console.log(response);
   }
--- frontend/src/landing-page/components/googleLogin.js
+++ frontend/src/landing-page/components/googleLogin.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
-export default function googleLogin() {
-    {console.log("Sending login request")}
-      axios.get(
-        "http://localhost:8000/admin/",
-        {
-          'login': 0,
-        }
-      );
-    //return await res.status;
+/**
+ * 
+ * @param {*} accesstoken This is the accesstoken of the user obtained from Google
+ */
+const googleLogin = async ({accesstoken}) => {
+    let res = await axios.post(
+      "http://localhost:8000/rest-auth/google/",
+      {
+        access_token: accesstoken,
+      }
+    );
+    console.log(res);
+    return await res.status;
   };
+
+export default googleLogin;
