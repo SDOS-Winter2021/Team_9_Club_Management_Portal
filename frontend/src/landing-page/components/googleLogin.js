36381acc6e9e7308dad95b96ea6703679304389b
Chokerino <bhavay18384@iiitd.ac.in>
2021-03-12 15:35:29 +0530

    added login request

--- frontend/src/landing-page/components/googleLogin.js
+++ frontend/src/landing-page/components/googleLogin.js
@@ -4,7 +4,7 @@ import axios from "axios";
  * 
  * @param {*} accesstoken This is the accesstoken of the user obtained from Google
  */
-const googleLogin = async ({accesstoken}) => {
+const googleLogin = async (accesstoken) => {
     let res = await axios.post(
       "http://localhost:8000/rest-auth/google/",
       {
