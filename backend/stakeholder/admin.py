08a8d84ad660a57c23e1c9666b444b34c8dc1798
peeyush-jain60 <62890098+peeyush-jain60@users.noreply.github.com>
2021-03-14 18:47:24 +0530

    addedclub apis

--- backend/stakeholder/admin.py
+++ backend/stakeholder/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-
+class CLUBAdmin(admin.ModelAdmin):
+	list_display=('name','date_time','location','description','payment_receipt_student','payment_receipt_reimburse','approved','attendance','club_name')
 # all club models
-admin.site.register(CLUB)
+admin.site.register(CLUB,CLUBAdmin)
 admin.site.register(Users)
 admin.site.register(CLUB_GENERAL)
 
