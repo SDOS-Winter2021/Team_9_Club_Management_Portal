bcfbd9bc99100db73df51c02de4df681b5cbea2d
Chokerino <bhavay18384@iiitd.ac.in>
2021-03-18 13:34:35 +0530

    looped

--- frontend/src/home-page/components/Clubs.js
+++ frontend/src/home-page/components/Clubs.js
@@ -52,87 +52,28 @@ class Clubs extends React.Component {
   render() {
     return (
       <div>
-        <Box
-          bg="#fffff"
-          border="2px"
-          width="90%"
-          ml="5%"
-          mb="2px"
-          flexDirection="row"
-        >
+        {
+        this.state.clubs.map((user, i) =>
+        <Box bg="#fffff" border="2px" width="90%" ml="5%" mb="2px" flexDirection="row">
           <Flex display="flex" flexDirection="row">
             {this.state.clubs.length !== 0 ? (
               <Text ml={3} mt={3} mb={3} fontWeight="bold">
-                {this.state.clubs[0]["club_name"]}
+                {this.state.clubs[i]["club_name"]}
               </Text>
             ) : null}
 
             {this.state.clubs.length !== 0 ? (
               <Text ml={3} mt={3} mb={3} fontWeight="light">
-                {this.state.clubs[0]["name"]}
+                {this.state.clubs[i]["name"]}
               </Text>
             ) : null}
 
-            <Image
-              ml="70%"
-              mt={3}
-              size="5px"
-              src="https://i.pinimg.com/originals/71/72/16/7172161b580470deb78078669236d2c1.jpg"
-              width="25px"
-              height="25px"
-            />
-            <Image
-              mt={3}
-              size="5px"
-              src="https://i.pinimg.com/originals/1d/ba/53/1dba53feeadf1a746f160396a6401135.png"
-              width="25px"
-              height="25px"
-            />
+            <Image ml="70%" mt={3} size="5px" src="https://i.pinimg.com/originals/71/72/16/7172161b580470deb78078669236d2c1.jpg" width="25px" height="25px"/>
+            <Image mt={3} size="5px" src="https://i.pinimg.com/originals/1d/ba/53/1dba53feeadf1a746f160396a6401135.png" width="25px" height="25px" />
           </Flex>
         </Box>
-
-        <Box
-          bg="#fffff"
-          border="2px"
-          width="90%"
-          ml="5%"
-          mb="2px"
-          flexDirection="row"
-        >
-          <Flex
-            display="flex"
-            flexDirection="row"
-            alignItems="flex-start"
-            justifyContent="flex-start"
-          >
-            {this.state.clubs.length !== 0 ? (
-              <Text ml={3} mt={3} mb={3} fontWeight="bold">
-                {this.state.clubs[1]["club_name"]}
-              </Text>
-            ) : null}
-
-            {this.state.clubs.length !== 0 ? (
-              <Text ml={3} mt={3} mb={3} fontWeight="light">
-                {this.state.clubs[1]["name"]}
-              </Text>
-            ) : null}
-            <Image
-              ml="70%"
-              mt={3}
-              size="5px"
-              src="https://i.pinimg.com/originals/71/72/16/7172161b580470deb78078669236d2c1.jpg"
-              width="25px"
-              height="25px"
-            />
-            <Image
-              mt={3}
-              size="5px"
-              src="https://i.pinimg.com/originals/1d/ba/53/1dba53feeadf1a746f160396a6401135.png"
-              width="25px"
-              height="25px"
-            />
-          </Flex>
-        </Box>
+      )
+      }
       </div>
     );
   }
--- frontend/src/home-page/components/Events.js
+++ frontend/src/home-page/components/Events.js
@@ -55,256 +55,43 @@ class Events extends React.Component {
         gap={6}
         templateColumns="repeat(auto-fit, minmax(350px, 1fr))"
       >
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="left"
-          justifyContent="center"
-          p={10}
-        >
-          {this.state.events.length !== 0 ? (
-            <Text
-              fontSize="lg"
-              lineHeight="normal"
-              textAlign="center"
-              mb={5}
-              color="twitter.900"
-              maxWidth="sm"
-            >
-              {this.state.events[0]["name"]}
-            </Text>
-          ) : null}
-
-          <Box
-            bg="#ffffff"
-            rounded="lg"
-            width="sm"
-            minHeight="sm"
-            border="1px solid lightgrey"
-            overflow="hidden"
-          >
+        {
+        this.state.events.map((user, i) =>
+        <Box display="flex" flexDirection="column" alignItems="left" justifyContent="center" p={10}>
+          <Box bg="#ffffff" rounded="lg" width="sm" minHeight="sm" border="1px solid lightgrey" overflow="hidden">
             <Box>
-              <Image
-                size="100px"
-                fallbackSrc="https://via.placeholder.com/150"
-                src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-                width="100%"
-                height="245px"
-                minHeight="245px"
-              />
+              <Image size="100px" fallbackSrc="https://via.placeholder.com/150" src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" width="100%" height="245px" minHeight="245px"/>
             </Box>
             <Box p={5} pb={8}>
-              <Box
-                display="flex"
-                justifyContent="flex-start"
-                alignItems="flex-start"
-                mb={1}
-                flexDirection="row"
-              >
-                <Badge
-                  variant="subtle"
-                  variantColor="teal"
-                  mr={2}
-                  rounded="lg"
-                  pl={2}
-                  pr={2}
-                >
+              <Box display="flex" justifyContent="flex-start" alignItems="flex-start" mb={1} flexDirection="row">
+                <Badge variant="subtle" variantColor="teal" mr={2} rounded="lg" pl={2} pr={2}>
                   NEW
                 </Badge>
               </Box>
               {this.state.events.length !== 0 ? (
                 <Text fontWeight="bold" fontSize="xl">
-                  {this.state.events[0]["description"]}
+                  {this.state.events[i]["description"]}
                 </Text>
               ) : null}
               {this.state.events.length !== 0 ? (
                 <Text fontSize="sm" mb={3}>
-                  {this.state.events[0]["club_name"]}
+                  {this.state.events[i]["club_name"]}
                 </Text>
               ) : null}
-              <Box
-                display="flex"
-                alignItems="center"
-                flexDirection="row"
-                justifyContent="flex-start"
-              >
-                <Icon name="star" color="yellow.400" mr={1} />
-                {this.state.events.length !== 0 ? (
+              <Box display="flex" alignItems="center" flexDirection="row" justifyContent="flex-start">
+                <Icon name="star" color="yellow.400" mr={1}/>
+                  {this.state.events.length !== 0 ? (
                   <Text fontWeight="bold" mr={1}>
-                    {this.state.events[0]["date_time"]}
+                    {this.state.events[i]["date_time"]}
                   </Text>
                 ) : null}
               </Box>
             </Box>
           </Box>
         </Box>
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="left"
-          justifyContent="center"
-          p={10}
-        >
-          {this.state.events.length !== 0 ? (
-            <Text
-              fontSize="lg"
-              lineHeight="normal"
-              textAlign="center"
-              mb={5}
-              color="twitter.900"
-              maxWidth="sm"
-            >
-              {this.state.events[0]["name"]}
-            </Text>
-          ) : null}
-
-          <Box
-            bg="#ffffff"
-            rounded="lg"
-            width="sm"
-            minHeight="sm"
-            border="1px solid lightgrey"
-            overflow="hidden"
-          >
-            <Box>
-              <Image
-                size="100px"
-                fallbackSrc="https://via.placeholder.com/150"
-                src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-                width="100%"
-                height="245px"
-                minHeight="245px"
-              />
-            </Box>
-            <Box p={5} pb={8}>
-              <Box
-                display="flex"
-                justifyContent="flex-start"
-                alignItems="flex-start"
-                mb={1}
-                flexDirection="row"
-              >
-                <Badge
-                  variant="subtle"
-                  variantColor="teal"
-                  mr={2}
-                  rounded="lg"
-                  pl={2}
-                  pr={2}
-                >
-                  NEW
-                </Badge>
-              </Box>
-              {this.state.events.length !== 0 ? (
-                <Text fontWeight="bold" fontSize="xl">
-                  {this.state.events[1]["description"]}
-                </Text>
-              ) : null}
-              {this.state.events.length !== 0 ? (
-                <Text fontSize="sm" mb={3}>
-                  {this.state.events[1]["club_name"]}
-                </Text>
-              ) : null}
-              <Box
-                display="flex"
-                alignItems="center"
-                flexDirection="row"
-                justifyContent="flex-start"
-              >
-                <Icon name="star" color="yellow.400" mr={1} />
-                {this.state.events.length !== 0 ? (
-                  <Text fontWeight="bold" mr={1}>
-                    {this.state.events[1]["date_time"]}
-                  </Text>
-                ) : null}
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="left"
-          justifyContent="center"
-          p={10}
-        >
-          {this.state.events.length !== 0 ? (
-            <Text
-              fontSize="lg"
-              lineHeight="normal"
-              textAlign="center"
-              mb={5}
-              color="twitter.900"
-              maxWidth="sm"
-            >
-              {this.state.events[1]["name"]}
-            </Text>
-          ) : null}
-
-          <Box
-            bg="#ffffff"
-            rounded="lg"
-            width="sm"
-            minHeight="sm"
-            border="1px solid lightgrey"
-            overflow="hidden"
-          >
-            <Box>
-              <Image
-                size="100px"
-                fallbackSrc="https://via.placeholder.com/150"
-                src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-                width="100%"
-                height="245px"
-                minHeight="245px"
-              />
-            </Box>
-            <Box p={5} pb={8}>
-              <Box
-                display="flex"
-                justifyContent="flex-start"
-                alignItems="flex-start"
-                mb={1}
-                flexDirection="row"
-              >
-                <Badge
-                  variant="subtle"
-                  variantColor="teal"
-                  mr={2}
-                  rounded="lg"
-                  pl={2}
-                  pr={2}
-                >
-                  NEW
-                </Badge>
-              </Box>
-              {this.state.events.length !== 0 ? (
-                <Text fontWeight="bold" fontSize="xl">
-                  {this.state.events[1]["description"]}
-                </Text>
-              ) : null}
-              {this.state.events.length !== 0 ? (
-                <Text fontSize="sm" mb={3}>
-                  {this.state.events[1]["club_name"]}
-                </Text>
-              ) : null}
-              <Box
-                display="flex"
-                alignItems="center"
-                flexDirection="row"
-                justifyContent="flex-start"
-              >
-                <Icon name="star" color="yellow.400" mr={1} />
-                {this.state.events.length !== 0 ? (
-                  <Text fontWeight="bold" mr={1}>
-                    {this.state.events[1]["date_time"]}
-                  </Text>
-                ) : null}
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-      </Grid>
+      )
+      } 
+    </Grid>
     );
   }
 }
