3e4059964beb3982423469df0764c07bc56b4f52
Chokerino <bhavay18384@iiitd.ac.in>
2021-04-09 19:45:11 -0700

    added unapproved events to home page for admins

--- CMS/frontend/src/home-page/components/Events.js
+++ CMS/frontend/src/home-page/components/Events.js
@@ -47,16 +47,25 @@ class Events extends React.Component {
 
   state = {
     events: [],
+    un_events: [],
   };
   componentDidMount() {
-    this.getEvents();
+    this.get_ApprovedEvents();
+    this.get_UnapprovedEvents();
   }
 
-  getEvents = () => {
+  get_ApprovedEvents = () => {
     console.log("Getting Events");
     axios
       .get("http://localhost:8000/api/clubs/unapproved")
       .then((data) => this.setState({ events: data.data }));
+  };
+  
+  get_UnapprovedEvents = () => {
+    console.log("Getting Events");
+    axios
+      .get("http://localhost:8000/api/clubs/unapproved")
+      .then((data) => this.setState({ un_events: data.data }));
   };
 
   Redirect_Event = (name) => {
@@ -77,6 +86,71 @@ class Events extends React.Component {
           gap={6}
           templateColumns="repeat(auto-fit, minmax(350px, 1fr))"
         >
+        {(() => {
+          if (sessionStorage.getItem("group") == "Admin" || sessionStorage.getItem("group") == "Club_Admin") {
+            return (
+              <>
+              {this.state.un_events.map((user, i) => (
+            <Stat
+              px={{ base: 4, sm: 6 }}
+              py="5"
+              bg={"gray.600"}
+              shadow="base"
+              rounded="lg"
+              key={this.state.un_events[i]["club_name"]}
+            >
+              <StatLabel
+                fontWeight="medium"
+                isTruncated
+                color={"white"}
+                fontSize="lg"
+              >
+                {this.state.un_events[i]["club_name"]}
+              </StatLabel>
+              <StatNumber fontSize="3xl" fontWeight="medium" color={"black"}>
+                <Button
+                  onClick={() =>
+                    this.Redirect_Event(this.state.un_events[i]["id"])
+                  }
+                  bg="white"
+                  fontSize="xl"
+                >
+                  {this.state.un_events[i]["name"]}
+                </Button>
+              </StatNumber>
+              <Stack direction={"row"} align={"center"}>
+                <Flex
+                  w={8}
+                  h={8}
+                  align={"center"}
+                  justify={"center"}
+                  rounded={"full"}
+                  bg={"gray.600"}
+                >
+                  <Icon as={IoAlarmOutline} color={"white"} w={5} h={5} />
+                </Flex>
+                  <Text fontWeight={600} color="white">
+                    {this.formatDate(this.state.events[i]["date_time"])}
+                  </Text>
+
+                <Flex
+                  w={8}
+                  h={8}
+                  align={"center"}
+                  justify={"center"}
+                  rounded={"full"}
+                  bg={"gray.600"}
+                >
+                  <Icon as={BsBuilding} color={"white"} w={5} h={5} />
+                </Flex>
+                <Text fontWeight={600} color="white">{this.state.un_events[i]["location"]}</Text>
+              </Stack>
+            </Stat>
+          ))}
+              </>
+         );
+        }
+      })()}
           {this.state.events.map((user, i) => (
             <Stat
               px={{ base: 4, sm: 6 }}
